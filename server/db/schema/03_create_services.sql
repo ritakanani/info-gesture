11a7aa35127bd0a2ef3030c76bf03ec535e64f18
knagayama76 <knagayama76>
2022-03-20 10:34:16 -0400

    update schema/seeds files

---
+++ server/db/schema/03_create_services.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS services CASCADE;
+
+CREATE TABLE services (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(255) NOT NULL,
+  category VARCHAR(255) NOT NULL,
+  description TEXT,
+  url VARCHAR(255),
+);
---
+++ server/db/seeds/03_services.sql
@@ -0,0 +1,9 @@
+-- test seeds for services
+INSERT INTO users (user_id, title, category, description, url)
+  VALUES (3, "Childcare for afterschool", "Childcare", "Donec vitae nisi. Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper", "")
+INSERT INTO users (user_id, title, category, description, url)
+  VALUES (4, "BEST HOUSE CLEANING", "Job", "Donec vitae nisi. Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper", "http://housecleaning.com")
+INSERT INTO users (user_id, title, category, description, url)
+  VALUES (5, "Foodbank", "social events", "Donec vitae nisi. Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper", "http://foodbank.com")
+INSERT INTO users (user_id, title, category, description, url)
+  VALUES (6, "Tutor for kids", "Job", "Donec vitae nisi. Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper", "http://bestteacher.com")
