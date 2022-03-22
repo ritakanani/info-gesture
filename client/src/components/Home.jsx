f217f181405a82c68c7970fe163c6a7f63617a38
Ritaben Vaghasia <ritakanani@yahoo.com>
2022-03-22 00:19:33 -0400

    Fix merge conflict

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 
 import { Container, Row, Col, Card, Button, Image } from "react-bootstrap";
@@ -8,7 +7,6 @@ import "./Home.scss";
 export default function Home(props) {
   return (
     <>
-      
       <Container className="main-title mb-5">
         <h2 className="fw-bold">Friendly Events & Services</h2>
       </Container>
@@ -95,101 +93,3 @@ export default function Home(props) {
     </>
   );
 }
-=======
-import React from "react";
-
-import { Container, Row, Col, Card, Button, Image } from "react-bootstrap";
-
-import "./Home.scss";
-
-export default function Home(props) {
-  return (
-    <>
-      
-      <Container className="main-title mb-5">
-        <h2 className="fw-bold">Friendly Events & Services</h2>
-      </Container>
-      <Container fluid className="bg-secondary d-flex justify-content-center">
-        <Row className="main-cards d-flex align-items-center">
-          <Col>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src="holder.js/100px180" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src="holder.js/100px180" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col>
-            <Card style={{ width: "18rem" }}>
-              <Card.Img variant="top" src="holder.js/100px180" />
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-      <Container fluid className="bg-secondary d-flex justify-content-center">
-        <Button>Learn more </Button>
-      </Container>
-      <Container>
-        <Row>
-          <Col>
-            <Image />
-          </Col>
-          <Col>
-            <h3>Events</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </p>
-            <Button>Create an Event</Button>
-          </Col>
-        </Row>
-      </Container>
-      <Container>
-        <Row>
-          <Col>
-            <h3>Services</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </p>
-            <Button>Provide Service</Button>
-          </Col>
-          <Col>
-            <Image />
-          </Col>
-        </Row>
-      </Container>
-    </>
-  );
-}
->>>>>>> service-component
--- client/src/components/Layout/Header.jsx
+++ client/src/components/Layout/Header.jsx
@@ -11,7 +11,6 @@ import {
 import { Link } from "react-router-dom";
 
 import "./Header.scss";
-<<<<<<< HEAD
 
 export default function Header(props) {
   return (
@@ -59,60 +58,6 @@ export default function Header(props) {
                     Services
                   </Link>
                 </Nav.Link>
-=======
-
-export default function Header(props) {
-  return (
-    <>
-       <Container fluid className="d-flex justify-content-end my-1">
-        <Link className="nav-menu mr-1" to="/login">
-          Login
-        </Link>
-        
-        <Link className="nav-menu text-light bg-secondary rounded-pill px-1" to="/register">
-          Register
-        </Link>
-      </Container>
-      <Navbar bg="light" expand="lg" className="py-4 border mb-5">
-        <Container fluid>
-          <Navbar.Brand className="nav-logo" href="#">
-            Info Gesture
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll" className="nav-menu">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: "100px" }}
-              navbarScroll
-            ></Nav>
-            <Container className="d-flex justify-content-end">
-              <div className="d-flex">
-                <Nav.Link>
-                  <Link className="nav-menu" to="/">
-                    Home
-                  </Link>
-                </Nav.Link>
-                <NavDropdown title="Events" id="navbarScrollingDropdown">
-                  <NavDropdown.Item href="#action3">Events</NavDropdown.Item>
-                  <NavDropdown.Item href="#action4">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action5">
-                    Something else here
-                  </NavDropdown.Item>
-                </NavDropdown>
-
-                <NavDropdown title="Services" id="navbarScrollingDropdown">
-                  <NavDropdown.Item href="/services">Services</NavDropdown.Item>    
-                  <NavDropdown.Item href="/create-service">Service Form</NavDropdown.Item> 
-               </NavDropdown>
-                {/* <Nav.Link>
-                  <Link className="nav-menu" to="/services">
-                    Services
-                  </Link>
-                </Nav.Link> */}
->>>>>>> service-component
               </div>
             </Container>
             <Form className="d-flex">
@@ -129,8 +74,4 @@ export default function Header(props) {
       </Navbar>
     </>
   );
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> service-component
