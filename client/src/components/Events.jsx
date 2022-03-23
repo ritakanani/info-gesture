409a24c5a69b410de6d7718d4a7b784156d39bfc
Ritaben Vaghasia <ritakanani@yahoo.com>
2022-03-22 20:19:25 -0400

    Solve error

--- client/src/components/Events.jsx
+++ client/src/components/Events.jsx
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import { Card, Button } from "react-bootstrap";
-
-const Events = () => {
-  return (
-    <>
-      <div>Event</div>
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src="" />
-        <Card.Body>
-          <Card.Title>Event Title</Card.Title>
-          <Card.Text>
-            March 22, 2022 •Toronto
-          </Card.Text>          
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-    </>
-  );
-};
-
-export default Events;
-=======
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Card, Button } from "react-bootstrap";
@@ -68,4 +40,3 @@ const Events = (props) => {
 };
 
 export default Events;
->>>>>>> fetch-data
